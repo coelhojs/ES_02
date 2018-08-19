4a807db14e5d99b87580b1a2b956c76a6a0db467
coelhojs <ccoelho.me@gmail.com>
2018-08-19 00:39:58 -0300

    ok

---
+++ LABORATÓRIO DE COMPUTAÇÃO II - 5104.1.01/Lista 00/src/Questão01_Fatorial.java
@@ -0,0 +1,17 @@
+public class Questão01_Fatorial {
+	public static int fatorial(int n) {
+		int fat;
+
+		if (n == 1 || n == 0) {
+			fat = 1;
+			return fat;
+		} else {
+			fat = n * (fatorial(n - 1));
+			return fat;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("O fatorial de 6 é: " + fatorial(6));
+	}
+}
