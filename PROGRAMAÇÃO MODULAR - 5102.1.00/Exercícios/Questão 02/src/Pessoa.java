c22f3d6a2f4134593b67e8d480c86000539c1c86
coelhojs <ccoelho.me@gmail.com>
2018-08-21 19:51:18 -0300

    ok

--- PROGRAMAÇÃO MODULAR - 5102.1.00/Exercícios/Questão 02/src/Pessoa.java
+++ PROGRAMAÇÃO MODULAR - 5102.1.00/Exercícios/Questão 02/src/Pessoa.java
@@ -1,8 +1,19 @@
 
 public class Pessoa {
+	private String nome;
+	private long cpf;
+	private int idade;
+	private String sexo;
+
+	public Pessoa(String nome, long cpf, int idade, String sexo) {
+		this.nome = nome; // "Cristiano";
+		this.cpf = cpf; // 11254085610L;
+		this.idade = idade; // 24;
+		this.sexo = sexo; // "masculino";
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
