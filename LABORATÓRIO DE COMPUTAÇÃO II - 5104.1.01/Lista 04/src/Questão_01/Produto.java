86ceec59b8561612d04ea580301c6747e9bac6f5
Cristiano Coelho De Jesus Souza <871826@sga.pucminas.br>
2018-08-30 22:13:59 -0300

    300820182213

---
+++ LABORATÓRIO DE COMPUTAÇÃO II - 5104.1.01/Lista 04/src/Questão_01/Produto.java
@@ -0,0 +1,23 @@
+package Questão_01;
+//Faça um programa que cadastre até 5 produtos. Para cada produtos devem ser cadastrados
+//código do produto, preço e quantidade estocada. Os dados devem ser armazenados em uma lista.
+//Posteriormente, receber do usuário a taxa de desconto (ex, digitar 10 para taxa de desconto de
+//10%). Aplicar a taxa digitada ao preço de todos os produtos cadastrados e finalmente mostrar
+//um relatório com o código e o novo preço. O final desse relatório deve apresentar também a
+//quantidade de produtos com quantidade estocada superior a 500.
+
+public class Produtos {
+	Produtos produtos = new Produtos(6);
+	
+	
+
+	
+	//void inserirInicio(elemento)
+	//void inserirFim(elemento)
+	//void inserir(elemento, posição)
+
+	//elemento removerInicio()
+	//elemento removerFim()
+	//elemento remover(posição)
+	//Mostrar, pesquisar, ordenar, ...
+}
