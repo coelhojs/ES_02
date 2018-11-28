b35c41cc8afb0579ac3e5ed0f720094c8f175772
coelhojs <ccoelho.me@gmail.com>
2018-11-27 23:47:20 -0200

    ok

---
+++ PROGRAMAÇÃO MODULAR - 5102.1.00/Exercícios/Lista 09/src/Questão_05/CheckOut.java
@@ -0,0 +1,20 @@
+package Questão_05;
+
+	void checkOut(Recibo recibo) {
+		Moeda total = Moeda.zero;
+	
+		for(item : items){
+			total += item.getPreco();
+			recibo.addItem(item);
+		}
+		
+		switch(opcaoPagto) {
+		case 1:
+			Pagamento dinheiro = aceitaDinheiro(total);
+			recibo.addPagamento(dinheiro);
+		break;
+		case 2:
+			Pagamento cartao = aceitaCartao(total);
+			recibo.addPagamento(cartao);	
+		}	
+	}
